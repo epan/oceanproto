fe6055bc890b0d91da981a154d46e1cabb80c1cd
epan <ericpan@gmail.com>
2012-11-15 14:39:10 -0800

    Cities list fixes
    100% image width, others

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-$(".button-prev").on('click', function(eventObject){
-	document.write('<a href="'+ document.referrer +'">jQuery</a>');
-});
+// TODO: Make the back buttons go to the prior page 
+// $(".button-prev").on('click', function(eventObject){
+// 	document.write('<a href="'+ document.referrer +'">jQuery</a>');
+// });
