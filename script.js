ba374de8b3515471e3059b29da164a072d9e8b0c
epan <ericpan@gmail.com>
2012-11-15 17:10:44 -0800

    Slider to city page (Boston)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // TODO: Make the back buttons go to the prior page 
-// $(".button-prev").on('click', function(eventObject){
-// 	document.write('<a href="'+ document.referrer +'">jQuery</a>');
-// });
+$(".button-prev").on('click', function(eventObject){
+	document.write('<a href="'+ document.referrer +'">jQuery</a>');
+});
