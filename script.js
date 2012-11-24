25867621f1d7d84aff4857416013191d849d8739
Slin Lee <slee@deepdyve.com>
2012-11-23 20:57:02 -0800

    Trying to make back button work.

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 // TODO: Make the back buttons go to the prior page 
-$(".button-prev").on('click', function(eventObject){
-	document.write('<a href="'+ document.referrer +'">jQuery</a>');
+// $(".button-prev").on('click', function(eventObject){
+// 	document.write('<a href="'+ document.referrer +'">jQuery</a>');
+// });
+
+$(document).ready(function() {
+        // $(".button-prev").attr('<a class="button-prev" href="' + document.referrer + '" data-transition="slide-out">Back</a>');
+        // $(".button-prev").attr("href", "document.referrer");
+        $(".button-prev").click(function() {
+            
+            alert("i'm ready!" + "document.referrer");
+            history.go(-1);
+        });
+        
 });
